02db11cf793c8a29db869d3fb3ff473c0055624e
Forrest Brown <forrestabrown@gmail.com>
2020-10-30 21:31:15 -0400

    feat: add home route

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+import { initPathRouter } from "@bjornlu/svelte-router";
+
 import App from "./App.svelte";
+import Home from "./Home.svelte";
+
+initPathRouter([
+	{ path: "/", component: Home }
+])
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: "world"
-	}
+	target: document.body
 });
 
 export default app;
